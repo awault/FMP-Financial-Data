68148349a297f5ebe10f3a639184e88f4bf95a09
Adam Ault <91794118+awault@users.noreply.github.com>
2025-02-14 21:17:59 +0000

    Create Function to download ohlc prices

--- src/downloader.py
+++ src/downloader.py
@@ -1,9 +1,13 @@
 # Functions used to download data
 
 import os
+import pandas as pd
+import requests
 from dotenv import load_dotenv
 load_dotenv()
 
+# Retrieve API key from the environment
+FMP_API_KEY = os.getenv("FMP_API_KEY")
 
 def check_api_key():
     """ Check if FMP API key is available in the environment variables."""
@@ -14,3 +18,25 @@ def check_api_key():
     else:
         print("API key is missing. Please set key value in the environment variables.")
         return False
+
+def fetch_fmp_data(ticker):
+    url= f'https://financialmodelingprep.com/api/v3/historical-price-full/{ticker}?apikey={FMP_API_KEY}'
+    
+    try:
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+        data = response.json()
+
+        if 'historical' in data:
+            #Flatten historical list into a DataFrame
+            historical_data = pd.DataFrame(data['historical'])
+            return historical_data
+        
+        elif isinstance(data,dict):
+            return pd.DataFrame([data])
+        
+        else:
+            print("Unexpected data format received")
+
+    except requests.RequestException as e:
+        print(f'Error fetching data: {e}')
