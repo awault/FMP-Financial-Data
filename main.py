dc1e5ce024260eca05d50ae75edc40f8660049d0
Adam Ault <91794118+awault@users.noreply.github.com>
2025-02-14 23:34:26 +0000

    Add downloader functions to main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+print("\n ~~ FMP Stock Price Data ~~ \n")
 import os
 import pandas as pd
 import requests
@@ -8,8 +9,47 @@ load_dotenv()
 from src.downloader import check_api_key
 check_api_key()
 
-# Fetch Data for AAPL
+# Import fetch fmp data function
 from src.downloader import fetch_fmp_data
-data = fetch_fmp_data('AAPL')
-print('\n')
-print(data.head())
+
+def main():
+    print("\nSelect an option to fetch stock price data:\n")
+    print("1. Enter a single ticker")
+    print("2. Enter a custom list of tickers")
+    print("3. Use the full NASDAQ ticker list")
+
+    choice = input("\nEnter your choice:").strip()
+
+    if choice == "1":
+        tickers = [input('Enter a single ticker:').strip().upper()]
+
+    elif choice == "2":
+        tickers = input('Enter tickers separated by commas: ').strip().upper().split(",")
+
+    elif choice == "3":
+        tickers = [NASDAQ]
+
+    else:
+        print("\nInvalid choice. Exiting program.\n")
+        return
+    
+    all_data = pd.DataFrame()
+
+    for ticker in tickers:
+        print(f"\nFetching data for {ticker}...")
+        df = fetch_fmp_data(ticker)
+        
+        if df is not None and not df.empty:
+            df['symbol'] = ticker
+            all_data = pd.concat([all_data,df], ignore_index=True)
+            
+    if not all_data.empty:
+            print(all_data.head())
+            
+    else:
+        print(f"Failed to fetch data for the provided tickers.")
+
+
+
+if __name__ == "__main__":
+    main()
